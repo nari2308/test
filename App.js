f70e86979e4a22cd1908469ca458b1ce0f3c53b0
nari2308 <138024540+nari2308@users.noreply.github.com>
2023-07-01 12:57:39 +0600

    updated

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter , Route, Routes } from 'react-router-dom'; 
+import { HashRouter, Route, Routes } from 'react-router-dom'; 
 import Signup from './Components/Signup/Signup';
 import Login from './Components/Login/Login';
 import Home  from './Components/Home/Home';
@@ -11,14 +11,14 @@ function App() {
     <>
     <Header />
     <Home />
-    <BrowserRouter basename={process.env.PUBLIC_URL + '/'}>
+    <HashRouter>
     <Routes>
       <Route exact path='/' Component={App}></Route>
       <Route  path='/test' Component={Home }></Route>
       <Route  path='/test/signup' Component={Signup }></Route>
      <Route  path='/test/login' Component={Login }></Route>
     </Routes>
-   </BrowserRouter>
+   </HashRouter>
    </>
   );
 }
